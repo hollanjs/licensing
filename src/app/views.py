3c079df3141b2b11ac1ace1ad225cf1878a7cc0d
hollanjs <jshollandsworth@gmail.com>
2016-02-05 14:08:29 -0500

    updated views.py to include compiler template and also licensors view to just show database list of the model entries. Added switchery to get that cool ios switch action on templates

--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+# remove later
+from django.views.generic import ListView
+###############
+
+from .models import Licensor
+
+
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html', {})
+
+def compiler(request):
+    return render(request, 'app/compiler.html', {})
+
+class LicensorList(ListView):
+    model = Licensor
