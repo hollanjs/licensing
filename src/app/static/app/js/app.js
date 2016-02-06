a55ccdbe697b84f3ce03a04958622879d67e3a04
Josh Hollandsworth <jshollandsworth@gmail.com>
2016-02-06 11:30:12 -0500

    made some small tweaks to css, js and html for compiler

--- src/app/static/app/js/app.js
+++ src/app/static/app/js/app.js
@@ -1,5 +1,7 @@
 $(function() {
 
+	// load up switchery to correctly show switches and animations
+
     console.log("loading switches for use!");
     var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
     elems.forEach(function(html) {
@@ -7,13 +9,18 @@ $(function() {
         });
     console.log("switches loaded!");
 
-    // sel_licensors = [];
+
+    // update licensing block with selected legal lines
 
     $(document).on('change', '.js-switch', function (event) {
     	selection = event.currentTarget;
     	$('.licensing-block #' + selection.id).toggle();
     });
 
+    // set [YEAR] to current year in licensing-block
 
+    var date = new Date();
+	var year = date.getFullYear();
+	$('.licensing-block').html($('.licensing-block').html().replace('[YEAR]', year));
 
 });
