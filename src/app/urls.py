f4ebdc125b3aa065ec04cb7d7b74b0448a9b9c0d
hollanjs <jshollandsworth@gmail.com>
2016-02-08 11:48:41 -0500

    setting up for inital deployment online

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^compiler/$', views.compiler, name = 'compiler'),
+    #url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.compiler, name = 'compiler'),
 ]
